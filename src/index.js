c05e3a79ed97126e08a122bfc00d489eb9cb9ec3
Daniel Jimenez <jimenezd305@gmail.com>
2019-01-02 10:02:26 -0500

    mv src/js to src/components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-import style from "./scss/app.scss";
-import App from "./js/App.jsx";
-
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
+import "./scss/app.scss";
+import App from "./components/App.jsx";
